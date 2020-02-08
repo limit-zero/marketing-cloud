068c2b6326f0475d6f8d513f28dcd1e70cfcb45c
Jacob Bare <jacob@limit0.io>
2020-02-08 11:53:36 -0600

    Dockerize graphql service

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -2,7 +2,6 @@ const {
   cleanEnv,
   validators,
   port,
-  num,
 } = require('@base-cms/env');
 
 const { nonemptystr } = validators;
@@ -12,6 +11,5 @@ module.exports = cleanEnv(process.env, {
   INTERNAL_PORT: port({ desc: 'The internal port that the server will run on.', default: 80 }),
   FUEL_API_CLIENT_ID: nonemptystr({ desc: 'The Marketing Cloud API client ID.' }),
   FUEL_API_CLIENT_SECRET: nonemptystr({ desc: 'The Marketing Cloud API client secret.' }),
-  FUEL_API_CLIENT_URL: nonemptystr({ desc: 'The Marketing Cloud URL to connect to, e.g. https://webservice.s7.exacttarget.com/Service.asmx' }),
-  EXACT_TARGET_CLIENT_ID: num({ desc: 'The Client.ID of the ExactTarget instance/BU.' }),
+  FUEL_API_WSDL: nonemptystr({ desc: 'The Marketing Cloud WSDL, e.g. https://webservice.s7.exacttarget.com/etframework.wsdl' }),
 });
