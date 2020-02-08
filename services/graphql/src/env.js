8133e1dd3567824530316b2edc3fa2b4302b97c3
Jacob Bare <jacob@limit0.io>
2020-02-08 16:33:17 -0600

    Update mc client settings

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -9,7 +9,7 @@ const { nonemptystr } = validators;
 module.exports = cleanEnv(process.env, {
   EXTERNAL_PORT: port({ desc: 'The external port that the server is exposed on.', default: 80 }),
   INTERNAL_PORT: port({ desc: 'The internal port that the server will run on.', default: 80 }),
+  FUEL_API_AUTH_URL: nonemptystr({ desc: 'The authentication base URI' }),
   FUEL_API_CLIENT_ID: nonemptystr({ desc: 'The Marketing Cloud API client ID.' }),
   FUEL_API_CLIENT_SECRET: nonemptystr({ desc: 'The Marketing Cloud API client secret.' }),
-  FUEL_API_WSDL: nonemptystr({ desc: 'The Marketing Cloud WSDL, e.g. https://webservice.s7.exacttarget.com/etframework.wsdl' }),
 });
--- services/graphql/src/mc.js
+++ services/graphql/src/mc.js
@@ -2,13 +2,11 @@ const MarkingCloudSOAP = require('@marketing-cloud/soap');
 const {
   FUEL_API_CLIENT_ID,
   FUEL_API_CLIENT_SECRET,
-  FUEL_API_WSDL,
+  FUEL_API_AUTH_URL,
 } = require('./env');
 
 module.exports = new MarkingCloudSOAP({
-  wsdl: FUEL_API_WSDL,
-  auth: {
-    clientId: FUEL_API_CLIENT_ID,
-    clientSecret: FUEL_API_CLIENT_SECRET,
-  },
+  clientId: FUEL_API_CLIENT_ID,
+  clientSecret: FUEL_API_CLIENT_SECRET,
+  authUrl: FUEL_API_AUTH_URL,
 });
