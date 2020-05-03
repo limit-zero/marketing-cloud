f46b8b73d2cf1d70fb71732d3899e4ad6750929e
Jacob Bare <jacob@limit0.io>
2020-05-03 09:53:53 -0500

    Use envalid directly

--- services/graphql/src/env.js
+++ services/graphql/src/env.js
@@ -1,17 +1,15 @@
 const {
   cleanEnv,
-  validators,
   port,
   num,
-} = require('@base-cms/env');
-
-const { nonemptystr } = validators;
+  str,
+} = require('envalid');
 
 module.exports = cleanEnv(process.env, {
   EXTERNAL_PORT: port({ desc: 'The external port that the server is exposed on.', default: 80 }),
   INTERNAL_PORT: port({ desc: 'The internal port that the server will run on.', default: 80 }),
-  FUEL_API_AUTH_URL: nonemptystr({ desc: 'The authentication base URI' }),
-  FUEL_API_CLIENT_ID: nonemptystr({ desc: 'The Marketing Cloud API client ID.' }),
-  FUEL_API_CLIENT_SECRET: nonemptystr({ desc: 'The Marketing Cloud API client secret.' }),
+  FUEL_API_AUTH_URL: str({ desc: 'The authentication base URI' }),
+  FUEL_API_CLIENT_ID: str({ desc: 'The Marketing Cloud API client ID.' }),
+  FUEL_API_CLIENT_SECRET: str({ desc: 'The Marketing Cloud API client secret.' }),
   FUEL_API_ACCOUNT_ID: num({ desc: 'Optional account identifier of the target business unit', default: undefined }),
 });
